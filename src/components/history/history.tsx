7246ff3c517a9646bfd5b0abce463bb5bf001668
Michel Gerding <michelgerding@gmail.com>
2023-06-12 13:27:21 +0200

    a

--- src/components/history/history.tsx
+++ src/components/history/history.tsx
@@ -1,4 +1,5 @@
 import { Moment } from "moment";
+import Image from "next/image";
 import React from "react";
 import Chart from "../Chart"
 
@@ -10,57 +11,101 @@ type HistoryProps = {
 
 export default function History({ location, filterFunction }: HistoryProps) {
 
+    const [colapsed, setColapsed] = React.useState(true)
+
+    const renderData = () => {
+        if (colapsed) {
+            return (
+
+                <div key={location.Location + ":header-wrapper"} style={{
+                    background: "#F6FCF7",
+                    padding: "30px 35px"
+                }}>
+                    <div style={{ margin: "0 0 10 0", display: "flex", justifyContent: "space-between" }}>
+                        <h2
+                            key={location.Location + ":header-chart"}
+                            style={{}}
+                        > {location.Location}</h2>
+                        <span onClick={() => {
+                            setColapsed(!colapsed);
+                        }}>  <Image style={{ marginTop: "10px" }} src="caret.svg" alt="" width={40} height={40} /> </span>
+
+                    </div>
+
+                    <div>
+                        <p style={{ margin: 0 }}> <b>Latest Humidity:</b> {location.measurements[0].humidity}</p>
+                        <p style={{ margin: 0 }}> <b>Latest Visibility:</b> {location.measurements[0].visib}</p>
+                    </div>
+
+
+                </div>
+            )
+        } else {
+            return (
+                <div key={location.Location + ":header-wrapper"} style={{
+                    background: "#F6FCF7",
+                    padding: "30px 35px"
+                }}>
+                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                        <h2
+                            key={location.Location + ":header-chart"}
+                            style={{ margin: "0 0 10 0" }}
+                        > {location.Location}</h2>
+                        <span onClick={() => {
+                            setColapsed(!colapsed);
+                        }}> X</span>
+
+                    </div>
+                    <h3 key={location.Location + ":header-humidity-chart"}
+                        style={{
+                            fontWeight: "normal",
+                            margin: "0"
+                        }}> Humidity </h3>
+                    <Chart
+                        key={location.Location + ":humidity-chart"}
+                        chartName="Humidity"
+                        data={location.measurements.map((d: any) => {
+                            return {
+                                x: d.Date,
+                                y: d.humidity
+                            }
+                        })}
+                        xType="date"
+                        yType="%"
+
+                        dataFilter={filterFunction}
+                    />
+
+                    <h3
+                        key={location.Location + ":header-visibility-chart"}
+                        style={{
+                            fontWeight: "normal",
+                            margin: "0"
+                        }}> Visibility </h3>
+                    <Chart
+                        key={location.Location + ":visibility-chart"}
+                        chartName="Visibility"
+                        data={location.measurements.map((d: any) => {
+                            return {
+                                x: d.Date,
+                                y: d.visib
+                            }
+                        })}
+                        xType="date"
+                        yType="km"
+                        dataFilter={(d: any) => {
+                            // if (d.y < ) 
+                            return true
+                        }}
+                    />
+                </div>
+            )
+        }
+    }
+
     return (
-        <div key={location.Location + ":header-wrapper"} style={{
-            background: "#F6FCF7",
-            padding: "30px 35px"
-        }}>
-            <h2
-                key={location.Location + ":header-chart"}
-                style={{ margin: "0 0 10 0" }}
-            > {location.Location}</h2>
-            <h3 key={location.Location + ":header-humidity-chart"}
-                style={{
-                    fontWeight: "normal",
-                    margin: "0"
-                }}> Humidity </h3>
-            <Chart
-                key={location.Location + ":humidity-chart"}
-                chartName="Humidity"
-                data={location.measurements.map((d: any) => {
-                    return {
-                        x: d.Date,
-                        y: d.humidity
-                    }
-                })}
-                xType="date"
-                yType="%"
-
-                dataFilter={filterFunction}
-            />
-
-            <h3
-                key={location.Location + ":header-visibility-chart"}
-                style={{
-                    fontWeight: "normal",
-                    margin: "0"
-                }}> Visibility </h3>
-            <Chart
-                key={location.Location + ":visibility-chart"}
-                chartName="Visibility"
-                data={location.measurements.map((d: any) => {
-                    return {
-                        x: d.Date,
-                        y: d.visib
-                    }
-                })}
-                xType="date"
-                yType="km"
-                dataFilter={(d: any) => {
-                    // if (d.y < ) 
-                    return true
-                }}
-            />
-        </div>
+        <>
+            {renderData()}
+        </>
     )
 }
